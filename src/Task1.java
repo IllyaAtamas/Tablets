9f6ee3f20ee04ea6d82575db96aeee7d4984140a
Ілля <128254803+IllyaAtamas@users.noreply.github.com>
2023-06-15 12:20:07 +0300

    without If-Else

--- src/Task1.java
+++ src/Task1.java
@@ -1,19 +1,13 @@
-import java.util.*;
-import java.util.stream.*;
-
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Task1 implements NameFormatter {
     @Override
     public String formatNames(List<String> names) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < names.size(); i++) {
-            if (i % 2 == 0) {
-                sb.append(i + 1).append(". ").append(names.get(i)).append(", ");
-            }
-        }
-        if (sb.length() > 2) {
-            sb.setLength(sb.length() - 2);  // Видалити останню кому та пробіл
-        }
-        return sb.toString();
+        return IntStream.range(0, names.size())
+                .filter(i -> i % 2 == 0)
+                .mapToObj(i -> (i + 1) + ". " + names.get(i))
+                .collect(Collectors.joining(", "));
     }
 }
